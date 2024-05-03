15def568addd228ba5a487c8ff1115ea9625ae2c
dev-Macintosh <ivan.200512340@gmail.com>
2024-05-03 22:32:51 +0300

    feat: Add MudBlazor library

--- Studentica.UI/Program.cs
+++ Studentica.UI/Program.cs
@@ -1,3 +1,4 @@
+using MudBlazor.Services;
 using Studentica.UI.Components;
 
 namespace Studentica.UI
@@ -8,17 +9,16 @@ namespace Studentica.UI
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
 
+            builder.Services.AddMudServices();
+
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
