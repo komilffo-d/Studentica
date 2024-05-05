a968214a5637985fa88be92dcadbce719935726e
komilffo-d <danil.200312340@gmail.com>
2024-05-05 20:16:49 +0300

    arch: Add LayoutBase class, setup breadcrumbs on every page

---
+++ Studentica.UI/Shared/Layout/LayoutBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Studentica.UI.Shared.Layout
+{
+    public abstract partial class LayoutBase: LayoutComponentBase
+    {
+        internal List<BreadcrumbItem> _links;
+
+        internal abstract List<BreadcrumbItem> Links { get; set; }
+    }
+}
