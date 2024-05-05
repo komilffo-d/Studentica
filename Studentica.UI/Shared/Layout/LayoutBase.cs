5f9c7e788751c02221074b850a3a914d10ea2c34
komilffo-d <danil.200312340@gmail.com>
2024-05-05 20:57:34 +0300

    chore: change modificator access field '_links' for LayoutBase and Update BuildAndDeployUI.yml

--- Studentica.UI/Shared/Layout/LayoutBase.cs
+++ Studentica.UI/Shared/Layout/LayoutBase.cs
@@ -5,7 +5,7 @@ namespace Studentica.UI.Shared.Layout
 {
     public abstract partial class LayoutBase: LayoutComponentBase
     {
-        internal List<BreadcrumbItem> _links;
+        private protected List<BreadcrumbItem> _links;
 
         internal abstract List<BreadcrumbItem> Links { get; set; }
     }
