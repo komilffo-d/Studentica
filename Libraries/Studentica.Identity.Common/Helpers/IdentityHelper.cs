2097038aab8d7f0432c40ffef64ea3051403db6e
komilffo-d <danil.200312340@gmail.com>
2024-06-07 22:59:00 +0300

    refactor: IdentityHelper

---
+++ Libraries/Studentica.Identity.Common/Helpers/IdentityHelper.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Studentica.Identity.Common.Helpers
+{
+    public static class IdentityHelper
+    {
+        private static string? _key;
+        public static SymmetricSecurityKey SecurityKey => new(
+            Encoding.ASCII.GetBytes(_key ?? throw new Exception("Ключ пустой!Используйте метод SetKey() для инициализации!")));
+
+        public static IServiceCollection AddJwtAuthentication(this IServiceCollection services)
+        {
+            services.AddAuthentication(o =>
+            {
+                o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(o =>
+                {
+                    o.RequireHttpsMetadata = false;
+                    o.SaveToken = true;
+                    o.TokenValidationParameters = GetValidationParameters();
+                }
+            );
+
+            return services;
+        }
+
+        public static TokenValidationParameters GetValidationParameters()
+        {
+            return new TokenValidationParameters()
+            {
+
+                ValidateIssuerSigningKey = true,
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                IssuerSigningKey = SecurityKey
+            };
+        }
+
+        public static void SetKey(string key)
+        {
+            _key = key;
+        }
+    }
+}
---
+++ Libraries/Studentica.Identity.Common/IdentitySettings.cs
@@ -0,0 +1,11 @@
+﻿using Studentica.Services.Common;
+
+namespace Studentica.Identity.Common
+{
+    public class IdentitySettings : ISettings
+    {
+        public string Key { get; init; } = null!;
+        public double AccessValidityMinutes { get; init; } = 15;
+        public double RefreshValidityDays { get; init; } = 30;
+    }
+}
